488e13c76bc72e5ade985abdd7db64455db9f68e
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-05-09 21:42:02 -0300

    aula 3 JS

---
+++ Pedro Patricio Rodrigues/WorkSpace/4 - JS/02_aula_JS/exercicio/main.js
@@ -0,0 +1,15 @@
+var nota1 = 7
+
+var nota2 = 9
+
+var nota3 = 5
+
+var nota4 = 8
+
+var soma = nota1 + nota2 + nota3 + nota4
+
+var media = soma / 4
+
+
+
+console.log("A media do aluno com as 4 notas é " + media)
---
+++ Pedro Patricio Rodrigues/WorkSpace/4 - JS/03_aula_JS/exercicio/main.js
@@ -0,0 +1,13 @@
+let fruta1 = "Banana"
+let fruta2 = "Melância"
+let fruta3 = "          Limão"
+let fruta4 = "Manga"
+let fruta5 = "Uva"
+
+console.log(fruta1.length)
+
+console.log(fruta2.substring(0,3))
+
+console.log(fruta3.trim())
+
+console
