27f836989efd449bc3e6a374931a296623905342
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-05-16 19:35:22 -0300

    aulas JS

--- Pedro Patricio Rodrigues/WorkSpace/4 - JS/03_aula_JS/exercicio/main.js
+++ Pedro Patricio Rodrigues/WorkSpace/4 - JS/03_aula_JS/exercicio/main.js
@@ -1,13 +1,22 @@
-let fruta1 = "Banana"
-let fruta2 = "Melância"
-let fruta3 = "          Limão"
-let fruta4 = "Manga"
-let fruta5 = "Uva"
+let fruta1 = "Banana";
+let fruta2 = "Melância";
+let fruta3 = "          Limão";
+let fruta4 = "Manga";
+let fruta5 = "Uva";
 
-console.log(fruta1.length)
+//length
+console.log(fruta1.length);
 
-console.log(fruta2.substring(0,3))
+//substring
+console.log(fruta2.substring(0,3));
 
-console.log(fruta3.trim())
+//trim
+console.log(fruta3.trim());
 
-console
+//replace - toLowerCase
+let novaMensagem = fruta4.replace('Manga', 'Pêra') ;
+console.log(novaMensagem.toLowerCase());
+
+//template string
+let templateString= 'Aqui temos 5 frutas: ${fruta1}, ${fruta2}, ${fruta3}, ${fruta4}, ${fruta5}';
+console.log(templateString)
