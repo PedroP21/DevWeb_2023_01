63357456ba62ad222c7e94a7f90948ca63a18d7f
PedroP21 <pedro.patricio.rds21@gmail.com>
2023-05-18 21:27:50 -0300

    up aula 08 JS

---
+++ Pedro Patricio Rodrigues/WorkSpace/4 - JS/08_aula_JS/exercicio/main.js
@@ -0,0 +1,22 @@
+alert("Seja bem vindo");
+
+let a = 3
+let b = 4
+let c = -5
+
+const x1 = (-b + Math.sqrt(delta)) / (2*a);
+const x2 = (-b - Math.sqrt(delta)) / (2*a);
+const delta = b * b - 4 * a * c;
+
+const eventClique = () => {
+    console.log('Você clicou no botão');
+};
+
+
+
+
+
+
+
+console.log(x1);
+console.log(x2
