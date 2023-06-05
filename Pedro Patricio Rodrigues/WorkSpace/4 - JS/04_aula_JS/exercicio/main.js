5be5485df272e4537795aaf16dd5e96564a1feaf
PedroP21 <pedro.patricio.rds21@gmail.com>
2023-06-04 23:50:03 -0300

    up exercicio 04 05

---
+++ Pedro Patricio Rodrigues/WorkSpace/4 - JS/04_aula_JS/exercicio/main.js
@@ -0,0 +1,9 @@
+var numero = 15;
+
+if (numero % 2 === 0) {
+    console.log("O número é par.");
+  } else {
+    console.log("O número é ímpar.");
+  }
+
+  console.log()
--- Pedro Patricio Rodrigues/WorkSpace/4 - JS/05_aula_JS/exercicio/main.js
+++ Pedro Patricio Rodrigues/WorkSpace/4 - JS/05_aula_JS/exercicio/main.js
@@ -5,4 +5,18 @@ while (contador <= 50) {
     contador++;
 }
 
-do-while
+
+let contador1 = 10;
+
+do {
+  console.log(`valor regressivo = ${contador1}`);
+  contador1--;
+} while (contador1 >= 0);
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+      console.log(`pares = ${i}`);
+    }
+  }
+  
